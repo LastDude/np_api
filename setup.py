5aa61193c6aa23d80eeefee0ae8212f4b5a5ee3b
Hans Peter <LetsPlayHansLP@gmail.com>
2018-07-28 15:28:52 +0200

    change

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ import setuptools
 setuptools.setup(
     name="np_api",
     version="0.0.1",
-    author="Bluefire",
-    author_email="author@example.com",
+    author="Hans Peter",
+    author_email="LetsPlayHansLP@gmail.com",
     description="A python wrapper for the unofficial nhentai api",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/LastDude/np_api",
     packages=["np_api"],
     license="MIT",
     classifiers=(
